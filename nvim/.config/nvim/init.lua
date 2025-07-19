9e299309537b2ea347b5963fd41dc96d8e45386b
Orlando <169166413+OrlandoDiaz2003@users.noreply.github.com>
2025-07-19 00:29:30 -0400

    nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,22 @@
+require("config.lazy")
+
+--opt
+vim.opt.shiftwidth = 3
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.relativenumber = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+--keymaps
+vim.keymap.set("n", "<Leader>pv", "<cmd>Ex<CR>")
+--lsp
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
