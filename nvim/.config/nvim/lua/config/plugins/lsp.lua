3992fd63fb2347a251c1c8b442c2e8153b47b57f
Orlando <169166413+OrlandoDiaz2003@users.noreply.github.com>
2025-07-19 14:20:15 -0400

    nvim html

--- nvim/.config/nvim/lua/config/plugins/lsp.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -12,7 +12,7 @@ return {
 	"mason-org/mason-lspconfig.nvim",
 	config = function ()
 	    require("mason-lspconfig").setup({
-		ensure_installed = { "clangd" }
+		ensure_installed = { "clangd", "pyright", "bashls", "html", "emmet_ls"}
 	    })
 	end
     },
@@ -37,6 +37,11 @@ return {
       config = function ()
 	 local capabilities = require('blink.cmp').get_lsp_capabilities()
 	 require("lspconfig").lua_ls.setup { capabilities = capabilities}
+	 require("lspconfig").pyright.setup { capabilities = capabilities }
+	 require("lspconfig").html.setup { capabilities = capabilities }
+	 require("lspconfig").emmet_ls.setup { vim.lsp.protocol.make_client_capabilities() }
+	 require("lspconfig").bashls.setup {}
+
       end,
    }
 
