760135289a346669cc2997c8d86166443ec60c2e
Orlando <ordiaz@duocuc.cl>
2025-09-10 01:54:43 -0300

    my keybinds for gnome

--- keybinds.sh
+++ keybinds.sh
@@ -1,5 +1,28 @@
   #!/bin/bash
 
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+	"['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>Return'
+
+# --- Super + D → Ocultar todas las apps (show desktop)
+gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
+
+# --- Super + E → Abrir file manager
+gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
+
+# --- Super + F → Pantalla completa
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
+
+# --- Super + Shift + Q → Cerrar app
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super><Shift>q']"
+
+# --- Alt + Tab → Cambiar entre ventanas directamente (no aplicaciones)
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+
 for i in {1..9}; do
     gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Super>$i']"
     gsettings set org.gnome.shell.keybindings switch-to-application-$i "[]"
